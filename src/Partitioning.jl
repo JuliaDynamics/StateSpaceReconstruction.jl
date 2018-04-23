8ac6544255d33b724eb6cf95b0e5aa940e5fe98c
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-04-23 20:38:25 +0200

    Partitioning module

---
+++ src/Partitioning.jl
@@ -0,0 +1,30 @@
+using Reexport
+@reexport module Partitioning
+
+using Parameters
+using ..Embeddings:
+			Embedding,
+			GenericEmbedding
+using GroupSlices:
+			groupslices,
+			firstinds,
+			groupinds
+using Simplices: Delaunay.delaunayn
+using SimplexSplitting:
+			centroids_radii2,
+			simplex_volumes,
+			orientations
+
+
+abstract type Partition end
+
+
+include("partitioning/rectangularbinning.jl")
+include("partitioning/triangulation.jl")
+
+
+export
+    Partition,
+    Triangulation, triangulate,
+    EquidistantBinning, bin_equidistant
+end
