d82303639248407053381f5739db248458bc31b0
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-04-23 20:52:11 +0200

    Main module

--- src/StateSpaceReconstruction.jl
+++ src/StateSpaceReconstruction.jl
@@ -1,5 +1,22 @@
 module StateSpaceReconstruction
 
-# package code goes here
+using Reexport
+using Parameters
+
+include("Timeseries.jl")
+include("Embeddings.jl")
+include("Partitioning.jl")
+
+# There might be many different types of state space reconstructions,
+# depending on what type of analysis we want to run, so define a
+# parent State Space Reconstruction (SSR) type
+abstract type SSR end
+
+struct GenericSSR
+    embedding::GenericEmbedding
+    partitions::Vector{Partition}
+end
+
+
 
 end # module
