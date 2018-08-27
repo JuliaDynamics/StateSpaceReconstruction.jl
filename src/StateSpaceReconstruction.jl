ad72c7b400be93e47dd089a1ab16f1029070a3d4
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-08-28 00:52:48 +0200

    Remove unnecessary SSR type

--- src/StateSpaceReconstruction.jl
+++ src/StateSpaceReconstruction.jl
@@ -5,21 +5,7 @@ module StateSpaceReconstruction
 using Reexport
 using Parameters
 
-include("TimeSeries.jl")
 include("Embeddings.jl")
 include("Partitioning.jl")
-
-
-# There might be many different types of state space reconstructions,
-# depending on what type of analysis we want to run, so define a
-# parent State Space Reconstruction (SSR) type
-abstract type SSR end
-
-struct GenericSSR
-    embedding::AbstractEmbedding
-    partitions::Vector{Partition}
-end
-
-
 
 end # module
