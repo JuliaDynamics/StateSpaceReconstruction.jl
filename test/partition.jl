bd3bc1024ae4b0f591f9bbac482fb53f2b818091
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-08-28 00:51:34 +0200

    Remove old rectangular binning tests

--- test/partition.jl
+++ test/partition.jl
@@ -42,23 +42,6 @@ end
     @test sum(m) ≈ 1
 end
 
-# @testset "Rectangular binning" begin
-#     n_pts = 30
-#     n_bins = 4
-#     E1 = embed([rand(n_pts) for i = 1:3])
-#     b1 = bin_rectangular(E1, n_bins) # bin sizes
-#     b2 = bin_rectangular(E1, 0.1) # bin sizes as fraction of ranges along each coord. axis
-#     b3 = bin_rectangular(E1, [0.1, 0.1, 0.2])
-#
-#     @test typeof(b1) <: RectangularBinning
-#     @test typeof(b2) <: RectangularBinning
-#     @test typeof(b3) <: RectangularBinning
-#
-#     E2 = embed([collect(1:10) for i = 1:3])
-#     b2 = bin_rectangular(E2, n_pts)
-#     @test typeof(b2) <: RectangularBinning
-# end
-
 @testset "Simplex triangulation" begin
     n_pts = 30
 	@testset "Triangulation" begin
