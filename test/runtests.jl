3b77112a8780202ed9fc5b896f0334885c04524a
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-11-09 10:16:45 +0100

    Configure PyCall and install scipy

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using StateSpaceReconstruction
 using Base.Test
 
+if lowercase(get(ENV, "CI", "false")) == "true"
+    include("install_dependencies.jl")
+end
+
 include("embedding_colwise.jl")
 include("partition.jl")
