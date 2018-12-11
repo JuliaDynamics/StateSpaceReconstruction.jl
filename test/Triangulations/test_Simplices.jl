9ca00e0930fa0dbb0ccc4f6e18723e801ea502c0
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-12-11 18:13:13 +0100

    Add constructors for vectors of vectors/static vectors + tests

--- test/Triangulations/test_Simplices.jl
+++ test/Triangulations/test_Simplices.jl
@@ -1,31 +1,67 @@
 
-using StateSpaceReconstruction
-using Test
+import StateSpaceReconstruction.Simplices
+import Test
 
 # Constructors
-@test Simplex([rand(3) for i = 1:4]) isa Simplex
-@test Simplex(rand(3, 4)) isa Simplex
-@test Simplex(rand(4, 3)) isa Simplex
-@test Simplex(rand(4, 5)) isa Simplex
-@test Simplex(rand(5, 4)) isa Simplex
-@test_throws DomainError Simplex(rand(5, 3))
+@testset "Immutable simplices" begin
+    @test Simplex([rand(3) for i = 1:4]) isa Simplex
+    @test Simplex(rand(3, 4)) isa Simplex
+    @test Simplex(rand(4, 3)) isa Simplex
+    @test Simplex(rand(4, 5)) isa Simplex
+    @test Simplex(rand(5, 4)) isa Simplex
+    @test Simplex([SVector{3}(rand(3)) for i = 1:4]) isa Simplex
 
-s = Simplex([rand(3) for i = 1:4])
+    @test_throws DomainError Simplex(rand(5, 3))
 
-@test npoints(s) == 4
-@test dimension(s) == 3
+    s = Simplex([rand(3) for i = 1:4])
 
-@test volume(s) isa Float64
-@test volume(s) >= 0
+    @test npoints(s) == 4
+    @test dimension(s) == 3
 
-@test orientation(s) isa Float64
+    @test volume(s) isa Float64
+    @test volume(s) >= 0
+
+    @test orientation(s) isa Float64
 
 
-s1 = Simplex(rand(3, 4))
-s2 = Simplex(rand(4, 3))
+    s1 = Simplex(rand(3, 4))
+    s2 = Simplex(rand(4, 3))
 
-# Intersection volume
-@test intersect(s1, s2) >= 0
+    # Intersection volume
+    @test intersect(s1, s2) >= 0
 
-# Intersecting vertices
-@test intersect_vertices(s1, s2) isa Array{Float64, 2}
+    # Intersecting vertices
+    @test intersect_vertices(s1, s2) isa Array{Float64, 2}
+end
+
+
+
+# Constructors
+@testset "Mutable simplices" begin
+    @test MutableSimplex([rand(3) for i = 1:4]) isa MutableSimplex
+    @test MutableSimplex([rand(3) for i = 1:4]) isa MutableSimplex
+    @test MutableSimplex([SVector{3}(rand(3)) for i = 1:4]) isa MutableSimplex
+
+    @test MutableSimplex(rand(3, 4)) isa MutableSimplex
+    @test MutableSimplex(rand(4, 3)) isa MutableSimplex
+    @test MutableSimplex(rand(4, 5)) isa MutableSimplex
+    @test MutableSimplex(rand(5, 4)) isa MutableSimplex
+    @test_throws DomainError MutableSimplex(rand(5, 3))
+
+    s = MutableSimplex([rand(3) for i = 1:4])
+
+    @test volume(s) isa Float64
+    @test volume(s) >= 0
+
+    @test orientation(s) isa Float64
+
+
+    s1 = MutableSimplex(rand(3, 4))
+    s2 = MutableSimplex(rand(4, 3))
+
+    # Intersection volume
+    @test intersect(s1, s2) >= 0
+
+    # Intersecting vertices
+    @test intersect_vertices(s1, s2) isa Array{Float64, 2}
+end
